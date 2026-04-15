1b0cbe15b01e69212cabbcfd06f3112ac1c85229
ScriptSmith24 <moritzmatthes990@gmail.com>
2026-04-15 15:39:32 +0200

    lala

--- server.c
+++ server.c
@@ -1,10 +1,10 @@
 #include <windows.h>
 #include <stdio.h>
-#include <winsock2.h>
+#include <winsock2.h> //-> Documentaion https://learn.microsoft.com/en-us/windows/win32/api/winsock2/
 #include <ws2tcpip.h>
 #pragma comment(lib, "ws2_32.lib") 
 
 
 int server(){
-
+    
 }
