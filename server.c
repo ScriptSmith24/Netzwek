ae5f444a74ee017a6b50e2ab4e5f369d338f4974
ScriptSmith24 <moritzmatthes990@gmail.com>
2026-04-16 12:00:50 +0200

    es gibt einen bug beim starten vom server

--- server.c
+++ server.c
@@ -1,71 +1,90 @@
-#include <windows.h>
-#include <stdio.h>
-#include <winsock2.h> //-> Documentaion https://learn.microsoft.com/en-us/windows/win32/api/winsock2/
+#include <winsock2.h>
 #include <ws2tcpip.h>
-#pragma comment(lib, "ws2_32.lib") 
+#include <stdio.h>
 
+#pragma comment(lib, "ws2_32.lib")
 
 int main() {
-    // 1. Create a data structure to hold Winsock info
     WSADATA wsa;
+    SOCKET s, new_socket;
+    struct sockaddr_in server, client;
+    int c;
 
-    printf("Initializing Winsock...\n");
-
-    // 2. Pass the version (2.2) and the address of your 'wsa' structure
-    // MAKEWORD(2,2) creates the version number needed
+    printf("Initialisiere Winsock...\n");
     if (WSAStartup(MAKEWORD(2,2), &wsa) != 0) {
-        printf("Failed. Error Code : %d\n", WSAGetLastError());
+        printf("Fehler: %d\n", WSAGetLastError());
         return 1;
     }
 
-    printf("Initialised successfully!\n");
-    SOCKET s;
-    s = socket(AF_INET, SOCK_STREAM, 0); // AF_INET = IPv4, SOCK_STREAM = TCP
-    if (s == INVALID_SOCKET) {
-        printf("Could not create socket : %d", WSAGetLastError());
+    // Socket erstellen
+    if((s = socket(AF_INET, SOCK_STREAM, 0)) == INVALID_SOCKET) {
+        printf("Socket Fehler: %d\n", WSAGetLastError());
+        return 1;
     }
-    struct sockaddr_in server;
+
     server.sin_family = AF_INET;
-    server.sin_addr.s_addr = INADDR_ANY; // Listen on all available network interfaces
-    server.sin_port = htons(8888);       // Port number (htons converts it to network byte order)
+    server.sin_addr.s_addr = INADDR_ANY;
+    server.sin_port = htons(8888);
 
-    if (bind(s, (struct sockaddr *)&server, sizeof(server)) == SOCKET_ERROR) {
-        printf("Bind failed with error code : %d", WSAGetLastError());
+    // Bind
+    if(bind(s, (struct sockaddr *)&server, sizeof(server)) == SOCKET_ERROR) {
+        printf("Bind Fehler: %d\n", WSAGetLastError());
+        closesocket(s);
+        WSACleanup();
+        return 1;
     }
 
-    listen(s, 3); // 3 is the "backlog" (how many people can wait in line)
+    // Listen
+    listen(s, 3);
+    printf("Server läuft auf Port 8888. Warte auf Verbindungen...\n");
 
-printf("Waiting for incoming connections...\n");
-
+    c = sizeof(struct sockaddr_in);
     while(1) {
-        struct sockaddr_in client;
-        int c = sizeof(struct sockaddr_in);
-        SOCKET new_socket = accept(s, (struct sockaddr *)&client, &c);
+        new_socket = accept(s, (struct sockaddr *)&client, &c);
+        if (new_socket == INVALID_SOCKET) continue;
 
-        if (new_socket == INVALID_SOCKET) {
-            printf("accept failed: %d\n", WSAGetLastError());
-            continue; // Keep trying for the next connection
+        char recv_buffer[1024] = {0};
+        recv(new_socket, recv_buffer, sizeof(recv_buffer), 0);
+
+        char method[10], path[100];
+        sscanf(recv_buffer, "%s %s", method, path);
+
+        char *fileName = path + 1; 
+        if (strcmp(path, "/") == 0) {
+            fileName = "Site.html";
         }
 
-        printf("Connection accepted!\n");
+        printf("Anfrage fuer: %s\n", fileName);
 
-        // The HTTP Response - Must have \r\n (Carriage Return + Line Feed)
-        char *response = "HTTP/1.1 200 OK\r\n"
-                         "Content-Type: text/html\r\n"
-                         "Content-Length: 35\r\n"
-                         "Connection: close\r\n"
-                         "\r\n"
-                         "<html><body>Hello World</body></html>";
+        FILE *file = fopen(fileName, "rb"); 
+        if (file == NULL) {
+            char *error = "HTTP/1.1 404 Not Found\r\n\r\n<h1>404 nicht gefunden</h1>";
+            send(new_socket, error, (int)strlen(error), 0);
+        } else {
+            char *contentType = "text/plain";
+            if (strstr(fileName, ".html")) contentType = "text/html";
+            else if (strstr(fileName, ".css")) contentType = "text/css";
+            else if (strstr(fileName, ".js"))  contentType = "application/javascript";
+            else if (strstr(fileName, ".png")) contentType = "image/png";
 
-        send(new_socket, response, (int)strlen(response), 0);
-        
-        // Close the specific client socket, but NOT the main listener 's'
+            char header[256];
+            sprintf(header, "HTTP/1.1 200 OK\r\nContent-Type: %s\r\nConnection: close\r\n\r\n", contentType);
+            send(new_socket, header, (int)strlen(header), 0);
+
+            // 4. Datei senden
+            char file_buffer[1024];
+            size_t bytes_read;
+            while ((bytes_read = fread(file_buffer, 1, sizeof(file_buffer), file)) > 0) {
+                send(new_socket, file_buffer, (int)bytes_read, 0);
+            }
+            fclose(file);
+        }
         closesocket(new_socket);
-        printf("Response sent and connection closed. Waiting for next...\n");
     }
 
-    // This part is rarely reached now because of while(1)
     closesocket(s);
-    WSACleanup();
+    WSACleanup();//end
     return 0;
 }
+
+
