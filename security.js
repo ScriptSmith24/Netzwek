400be383668b4b48e4ac8d26a64de26e36ad617f
FriedemannGrothe <66796934+FriedemannGrothe@users.noreply.github.com>
2026-06-03 08:50:37 +0200

    removed getencryptionvariable function - not needed overhead

--- security.js
+++ security.js
@@ -1,13 +1,9 @@
-function get_encryptionVariable(username){
-    return username.length
-}
-
 function Caesar_Encrypt(username, messageText) {
-    messageText = messageText + get_encryptionVariable(username);
+    messageText = messageText + username.length;
     return messageText
 }
 
 function Caesar_Decrypt(username, messageText) {
-    messageText = messageText - get_encryptionVariable(username);
+    messageText = messageText - username.length;
     return messageText
 }
